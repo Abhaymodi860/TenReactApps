bf5f1715ef454165c872ffe2089217cff2882d50
Abhaymodi860 <modiabhay860@gmail.com>
2023-05-07 18:15:32 +0530

    02-display-array-of-users

--- ten-react-app/src/App.js
+++ ten-react-app/src/App.js
@@ -1,9 +1,23 @@
 import "./App.css";
 
 function App() {
+  // the array/list of users with name and id
+  const users = [
+    { id: 1, name: "Leon" },
+    { id: 2, name: "Ada" },
+    { id: 3, name: "Ashley" },
+  ];
+
+  // we use map function to render the elements of array not for loop or any other.
+  const userItems = users.map((user) => <li key={user.id}>{user.name}</li>);
+
   return (
     <div>
-      <h1>Ten React App</h1>
+      <h1>App 2: Display array of users</h1>
+      <div>
+        <h3>Users name:</h3>
+        <ul>{userItems}</ul>
+      </div>
     </div>
   );
 }
