874d99eecc68dd482a9a3c73717cc1f62ac8c90a
Abhaymodi860 <modiabhay860@gmail.com>
2023-05-07 22:30:08 +0530

    03-show-hide-element

--- ten-react-app/src/App.js
+++ ten-react-app/src/App.js
@@ -1,9 +1,18 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [show, setShow] = useState(true);
+
   return (
     <div>
-      <h1>Ten React App</h1>
+      <h1>App 3: Show/Hide an element</h1>
+      <div>
+        <button onClick={() => setShow(!show)}>
+          {show ? "Hide Element" : "Show Element"}
+        </button>
+        {show && <div>Toggle change</div>}
+      </div>
     </div>
   );
 }
