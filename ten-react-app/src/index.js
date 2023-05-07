d23a731e5773328cf2e8b83c702eb827329f08c5
Abhaymodi860 <modiabhay860@gmail.com>
2023-05-07 17:10:08 +0530

    01-display-simple-jsx

--- ten-react-app/src/index.js
+++ ten-react-app/src/index.js
@@ -1,8 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// This function is a component that is rendered
+// all the tags inside are JSX not HTML.
+const App = () => {
+  return (
+    <div>
+      <h1>App 1: Displaying JSX</h1>
+      <h3>JSX is Cool!</h3>
+    </div>
+  );
+};
 root.render(
   <React.StrictMode>
     <App />
