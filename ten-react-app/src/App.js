1237cd80fbbfb60b0921fa48a4b5f4aa21b35079
Abhaymodi860 <modiabhay860@gmail.com>
2023-05-09 19:26:22 +0530

    04-two-way-data-binding

--- ten-react-app/src/App.js
+++ ten-react-app/src/App.js
@@ -1,9 +1,21 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [value, setValue] = useState("");
   return (
     <div>
-      <h1>Ten React App</h1>
+      <h1>App 4: two way data binding</h1>
+      <div>
+        {/* the set value function in onChange will update the value variable in p on every render */}
+        <input
+          type="text"
+          placeholder="Enter Text"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+      </div>
+      <p>{value}</p>
     </div>
   );
 }
