f46a0d4628b4526a80800f41297df88b0df4f58b
Abhaymodi860 <modiabhay860@gmail.com>
2023-05-10 11:56:23 +0530

    05-disable-button-if-empty

--- ten-react-app/src/App.js
+++ ten-react-app/src/App.js
@@ -1,9 +1,16 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [value, setValue] = useState("");
+
   return (
     <div>
-      <h1>Ten React App</h1>
+      <h1>App 5: Disable button is input is empty on less than 5 letters</h1>
+      <div>
+        <input type="text" onChange={(e) => setValue(e.target.value)} />
+        <button disabled={value.length < 5}>Submit</button>
+      </div>
     </div>
   );
 }
