5e2cca60b4bb9e4b41df6ce5de4a7d75e2881225
Abhaymodi860 <modiabhay860@gmail.com>
2023-05-11 12:13:35 +0530

    07-dynamically-adding-child-component

--- ten-react-app/src/App.js
+++ ten-react-app/src/App.js
@@ -1,9 +1,29 @@
 import "./App.css";
 
+function Child() {
+  return (
+    <div>
+      <h3>This is child component.</h3>
+    </div>
+  );
+}
+
+function Parent({ children }) {
+  return (
+    <div>
+      <h2>This is parent component.</h2>
+      {children}
+    </div>
+  );
+}
+
 function App() {
   return (
     <div>
-      <h1>Ten React App</h1>
+      <h1>App 7: Dynamically Adding child component</h1>
+      <Parent>
+        <Child />
+      </Parent>
     </div>
   );
 }
